99982b88ed5027c0df8151cbf51b042d14962b72
Gabriel Veliz <gvelizzuniga@gmail.com>
2024-07-07 20:18:26 -0400

    fin desafio

--- js/script.js
+++ js/script.js
@@ -28,25 +28,17 @@ Extras:
 Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
 */
 
-
+var valorcopiar = "";
 function encriptar(){
 
     var array = [];
 
-    document.getElementById("imagenlupa").style.display="none";
-    document.querySelector('#texto_informacion1').innerHTML=""
-    document.querySelector('#texto_informacion2').innerHTML=""
+    esconder();
 
 
     let texto = document.getElementById('textinput').value
     if(texto==""){
-        document.getElementById("imagenlupa").style.display="block";
-        document.querySelector('#imagenlupa').src="./Assets/mujer_lupa.PNG"
-        document.querySelector('#texto_informacion1').innerHTML="Ningún mensaje fue encontrado"
-        document.querySelector('#texto_informacion2').innerHTML="Ingresa el texto que desees encriptar o desencriptar."
-        document.querySelector('#div_des').classList.add('cont_tex_des')
-        document.querySelector('#div_des').classList.remove('cont_tex_des2')
-        document.querySelector("#texto_en").innerHTML="";
+        no_encontrado();
     }else{
         for(let i=0;i<texto.length;i++){
             if(texto[i]=="e"){
@@ -64,12 +56,61 @@ function encriptar(){
             }
         }
         document.querySelector("#texto_en").innerHTML=array.toString().replace(/,/g,"");
-        document.querySelector('#div_des').classList.remove('cont_tex_des')
-        document.querySelector('#div_des').classList.add('cont_tex_des2')
+        valorcopiar=array.toString().replace(/,/g,"");
+        cambio_clase();
+    }
+
+}
+function esconder(){
+    document.getElementById("imagenlupa").style.display="none";
+    document.querySelector('#texto_informacion1').innerHTML=""
+    document.querySelector('#texto_informacion2').innerHTML=""
+    document.getElementById("id_btn_copiar").style.display="flex";
+}
+function no_encontrado(){
+    document.getElementById("imagenlupa").style.display="block";
+    document.querySelector('#imagenlupa').src="./Assets/mujer_lupa.PNG"
+    document.querySelector('#texto_informacion1').innerHTML="Ningún mensaje fue encontrado"
+    document.querySelector('#texto_informacion2').innerHTML="Ingresa el texto que desees encriptar o desencriptar."
+    document.querySelector('#div_des').classList.add('cont_tex_des')
+    document.querySelector('#div_des').classList.remove('cont_tex_des2')
+    document.querySelector("#texto_en").innerHTML="";
+    document.getElementById("id_btn_copiar").style.display="none";
+}
+function cambio_clase(){
+    document.querySelector('#div_des').classList.remove('cont_tex_des')
+    document.querySelector('#div_des').classList.add('cont_tex_des2')
+}
+function desencriptar(){
+    esconder();
+    let texto = document.getElementById('textinput').value
+
+    if(texto==""){
+        no_encontrado();
+    }
+    else{
+        texto =texto.replace(/enter/g,"e");
+        texto =texto.replace(/imes/g,"i");
+        texto =texto.replace(/ai/g,"a");
+        texto =texto.replace(/ober/g,"o");
+        texto =texto.replace(/ufat/g,"u");
+        document.querySelector("#texto_en").innerHTML=texto;
+        valorcopiar=texto;
+        cambio_clase();
     }
 
 }
 
-function desencriptar(){
+function copiar() {
+    navigator.clipboard.writeText(valorcopiar)
+}
+function corregirtexto(event) {
+    // Obtener el valor del textarea
+    let input = event.target.value;
     
+    // Convertir a minúsculas y eliminar caracteres especiales
+    let transformed = input.toLowerCase().replace(/[^a-z0-9\s]/g, '');
+    
+    // Asignar el valor transformado al textarea
+    event.target.value = transformed;
 }
